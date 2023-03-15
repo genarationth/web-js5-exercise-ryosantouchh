04128ab3b298893864a5963bb40cdf6661056bbc
ryosantouchh <kittitat.suntimak@gmail.com>
2023-03-15 21:29:59 +0700

    add nested loop for exercise2

--- exercise2.js
+++ exercise2.js
@@ -1,3 +1,4 @@
+// best practice
 const addStar = (x) => {
   let temp = "";
   for (let i = 0; i < x; i++) {
@@ -5,3 +6,12 @@ const addStar = (x) => {
   }
 };
 addStar(5);
+
+// nested loop
+for (let i = 0; i < 5; i++) {
+  let temp = "";
+  for (let j = 0; j < i + 1; j++) {
+    temp += "*";
+  }
+  console.log(temp);
+}
